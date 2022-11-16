b365443bca6b7a1b04ef8d2bbf6697764c760e72
rangalakshmi <ubuntu@ranga.domain>
2022-11-16 02:29:21 +0000

    changes for1.sh

--- for1.sh
+++ for1.sh
@@ -3,7 +3,7 @@ str="Employee prfession dept salary age
 ajay manager account 45000 70
 sunil clerk account 25000 60
 varun manager sales 50000 75
-amit manager account 47000 85"
+echo ""amit manager account 47000 85"
 
 for i in $str
 	do
